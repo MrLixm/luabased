e947d10a497b823ad0586650a1a9d14e70e0778a
Liam Collod <lcollod@gmail.com>
2022-09-08 20:05:03 +0200

    refactoring `formatting` module to `stringing`

--- luabased/stringing.lua
+++ luabased/stringing.lua
@@ -1,6 +1,5 @@
-local mathing = require("luabased.mathing")
-local StrFmtSettings =  require("luabased.formatting_settings")
 local _M = {}
+local mathing = require("luabased.mathing")
 
 -- we make some global functions local as this will improve performances in
 -- heavy loops.
@@ -10,6 +9,10 @@ local tableconcat = table.concat
 local tableinsert = table.insert
 local stringrep = string.rep
 local type = type
+
+-- pre-declared objects
+local StringFormattingSettings = {}
+
 
 function _M.conkat(...)
   --[[
@@ -53,7 +56,7 @@ function _M:stringify(source, index, settings)
     settings(StrFmtSettings or nil): configure how source is formatted
   ]]
   if not settings then
-    settings = StrFmtSettings:new()
+    settings = StringFormattingSettings:new()
   end
 
   if not index then
@@ -112,7 +115,7 @@ function _M:table2string(tablevalue, index, settings)
   if settings and settings.tables then
     tsettings = settings.tables
   else
-    tsettings = StrFmtSettings:new().tables
+    tsettings = StringFormattingSettings:new().tables
   end
 
   local linebreak_start = "\n"
@@ -170,5 +173,92 @@ function _M:table2string(tablevalue, index, settings)
   return tostring(tableconcat(outtable))
 
 end
+
+
+function StringFormattingSettings:new()
+  --[[
+  A base class that hold configuration settings for string formatting used
+  by stringify() and table2string()
+  ]]
+
+  -- these are the default values
+  local attrs = {
+    ["display_time"] = true,
+    ["display_context"] = true,
+    ["blocks_duplicate"] = true,
+    -- how much decimals should be kept for floating point numbers
+    ["numbers"] = {
+      ["round"] = 3
+    },
+    -- nil by default cause the table2string already have some defaults
+    ["tables"] = {
+      -- how much whitespaces is considered an indent
+      ["indent"] = 4,
+      -- max table size before displaying it as oneline to avoid flooding
+      ["length_max"] = 50,
+      -- true to display the table on multiples lines with indents
+      ["linebreaks"] = true,
+      ["display_indexes"] = false,
+      ["display_functions"] = true
+    },
+    ["strings"] = {
+      ["display_quotes"] = false
+    }
+  }
+
+  function attrs:set_display_time(enable)
+    -- enable(bool): true to display the current time as h:m:s
+    self.display_time = enable
+  end
+
+  function attrs:set_display_context(enable)
+    -- enable(bool): true to display Logger.ctx
+    self.display_context = enable
+  end
+
+  function attrs:set_blocks_duplicate(enable)
+    -- enable(bool): true to enable blocking of repeated messages
+    self.blocks_duplicate = enable
+  end
+
+  function attrs:set_num_round(round_value)
+    -- round_value(int):
+    self.numbers.round = round_value
+  end
+
+  function attrs:set_str_display_quotes(display_value)
+    -- display_value(bool):
+    self.strings.display_quotes = display_value
+  end
+
+  function attrs:set_tbl_display_indexes(display_value)
+    -- display_value(bool):
+    self.tables.display_indexes = display_value
+  end
+
+  function attrs:set_tbl_linebreaks(display_value)
+    -- display_value(bool):
+    self.tables.linebreaks = display_value
+  end
+
+  function attrs:set_tbl_length_max(length_max)
+    -- length_max(int):
+    self.tables.length_max = length_max
+  end
+
+  function attrs:set_tbl_indent(indent)
+    -- indent(int):
+    self.tables.indent = indent
+  end
+
+  function attrs:set_tbl_display_functions(display_value)
+    -- display_value(bool):
+    self.tables.display_functions = display_value
+  end
+
+  return attrs
+
+end
+
 
 return _M
