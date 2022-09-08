468680e4385b27fe9cef89048e6db62417285046
Liam Collod <lcollod@gmail.com>
2022-09-08 19:23:13 +0200

    proper lua package declaration

---
+++ luabased/init.lua
@@ -0,0 +1,26 @@
+local _M_ = {}
+_M_._VERSION = '0.1.0'
+_M_._DESCRIPTION = 'Extending the base lua library with missing features.'
+_M_._LICENSE = [[
+   Copyright 2022 Liam Collod
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+]]
+
+local _self = (...):gsub('%.init$', '')
+
+_M_.formatting = require(_self .. '.formatting')
+_M_.formatting_settings = require(_self .. '.formatting_settings')
+_M_.mathing = require(_self .. '.mathing')
+
+return _M_
